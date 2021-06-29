1446d32898b11a4ee816e118d4b7d454b2e51fca
tosebikan <tosebikan@gmail.com>
2021-06-29 17:04:18 +0000

    final

---
+++ components/Draggable.js
@@ -0,0 +1,79 @@
+import { PanResponder } from 'react-native';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default class Draggable extends React.Component {
+  static propTypes = {
+    children: PropTypes.func.isRequired,
+    onTouchStart: PropTypes.func,
+    onTouchMove: PropTypes.func,
+    onTouchEnd: PropTypes.func,
+    enabled: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    onTouchStart: () => {},
+    onTouchMove: () => {},
+    onTouchEnd: () => {},
+    enabled: true,
+  };
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      dragging : false
+    };
+
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: this.handleStartShouldSetPanResponder,
+      onPanResponderGrant: this.handlePanResponderGrant,
+      onPanResponderMove: this.handlePanResponderMove,
+      onPanResponderRelease: this.handlePanResponderEnd,
+      onPanResponderTerminate: this.handlePanResponderEnd,
+
+    });
+  }
+
+  handleStartShouldSetPanResponder = () => {
+    const {enabled} = this.props;
+    return enabled;
+  }
+
+  handlePanResponderGrant = () => {
+    const {onTouchStart} = this.props;
+    this.setState({ dragging : true});
+    onTouchStart();
+  }
+
+  handlePanResponderMove = (e, gestureState) => {
+    const {onTouchMove} = this.props;
+    const offset = {
+      top: gestureState.dy,
+      left: gestureState.dx,
+    };
+    onTouchMove(offset);
+  }
+
+  handlePanResponderEnd = (e, gestureState) => {
+    const {onTouchMove, onTouchEnd} = this.props;
+    this.setState({ draggng : false});
+
+    const offset = {
+      top: gestureState.dy,
+      left: gestureState.dx
+    };
+
+    onTouchMove(offset);
+    onTouchEnd(offset);
+  }
+
+  render() {
+    const {children} = this.props;
+    const {dragging} = this.state;
+    return children({
+      handlers: this.panResponder.panHandlers,
+      dragging
+    });
+  }
+}
